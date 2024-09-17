c90f10ae80ccfbff6efb7282ba4da2d88471a672
damjaguar98@icloud.com <damjaguar98@icloud.com>
2024-09-17 15:27:22 +0200

    Edit Responsive and Animation

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -1,28 +1,44 @@
-
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function Hero() {
   return (
-    <section className='w-screen py-20 flex items-center  max-lg:h-auto max-md:flex-col-reverse max-sm:py-10'>
-        <div className='w-1/2 flex justify-center max-xl:justify-start max-xl:pl-20 max-md:w-full max-md:mt-10 max-sm:pl-10'>
-            <div className=''>
-                <h1 className='font-bold text-5xl max-sm:text-3xl'>Hi, <br />I am Damiano Ciucci</h1>
-                <p className='mt-2 text-textSubHero text-fontSubHero max-sm:text-sm'>Full stack developer</p>
-                <div className='flex gap-3 mt-9 max-sm:mt-6'>
-                    <div className='w-36 h-10 flex justify-center items-center bg-buttonColor hover:scale-105 max-sm:w-32'><Link href={'/giacomo.jpg'} className='text-sm'>Download CV</Link></div>
-                    <div className='w-36 h-10 flex justify-center items-center border border-gray-300 hover:scale-105 max-sm:w-32'><Link href={'#about'} className='text-sm'>Learn more</Link></div>
-                </div>
+    <section className="w-screen py-20 flex items-center  max-lg:h-auto max-md:flex-col-reverse max-sm:py-10 main-container-xl">
+      <div className="w-1/2 flex justify-center max-xl:justify-start max-xl:pl-20 max-md:w-full max-md:mt-10 max-sm:pl-10">
+        <div className="">
+          <h1 className="font-bold text-5xl max-sm:text-3xl">
+            Hi, <br />I am Giacomo Roberti
+          </h1>
+          <p className="mt-2 text-textSubHero text-fontSubHero max-sm:text-sm">
+            Voice Actor
+          </p>
+          <div className="flex gap-3 mt-9 max-sm:mt-6">
+            <div className="w-36 h-10 flex justify-center items-center bg-buttonColor hover:scale-105 max-sm:w-32">
+              <Link href={"/giacomo.jpg"} className="text-sm">
+                Download CV
+              </Link>
             </div>
+            <div className="w-36 h-10 flex justify-center items-center border border-gray-300 hover:scale-105 max-sm:w-32">
+              <Link href={"#about"} className="text-sm">
+                Learn more
+              </Link>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div className='w-1/2 flex justify-center max-md:w-full'>
-            <div className='circle-container'>
-                <Image src={'/volto.png'} width={597} height={597} alt='img-hero' className='rounded-roundedImg max-xl:w-96 max-xl:h-96 max-lg:max-w-80 max-lg:h-80  max-sm:w-56 max-sm:h-56'></Image>
-            </div>
+      <div className="w-1/2 flex justify-center max-md:w-full">
+        <div className="circle-container">
+          <Image
+            src={"/volto.png"}
+            width={597}
+            height={597}
+            alt="img-hero"
+            className="rounded-roundedImg max-xl:w-96 max-xl:h-96 max-lg:max-w-80 max-lg:h-80  max-sm:w-56 max-sm:h-56"
+          ></Image>
         </div>
+      </div>
     </section>
-  )
+  );
 }
--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -1,53 +1,72 @@
+"use client";
 
-'use client';
-
-import Link from 'next/link'
-import React, { useState , useEffect} from 'react'
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
 import { IoIosMenu } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
 
 export default function Navbar() {
-    const[isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
 
-    useEffect(()=>{
-
-      const handleScroll = ()=>
-      {
-          if(window.scrollY > 100)
-          {
-              setIsOpen(false);
-          }
-
-         
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsOpen(false);
       }
+    };
 
-      window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-      return ()=>{
-          window.removeEventListener('scroll', handleScroll);
-      }
-      
-  },[]);
-  
-  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleMenuToggle = () => {
+    if (isOpen) {
+      setIsClosing(true);
+      setTimeout(() => {
+        setIsOpen(false);
+        setIsClosing(false);
+      }, 300);
+    } else {
+      setIsOpen(true);
+    }
+  };
+
   return (
-    <nav className='w-screen h-16  flex items-center z-50 sticky backdrop-blur-sm bg-NavColor'>
-        <div className='flex items-center w-full justify-between px-20 max-sm:px-10'>
-            <h1 className='font-bold text-4xl max-md:text-xl'>Damiano Ciucci</h1>
-            <div className='flex gap-10 max-md:hidden'>
-                <Link href={'#about'}>About me</Link>
-                <Link href={'#servizi'}>Sevices</Link>
-                <Link href={'#contatti'}>Contact me</Link>
-            </div>
-            {!isOpen ? <IoIosMenu className='w-8 h-8  cursor-pointer hidden max-md:block' onClick={()=>setIsOpen(!isOpen)}></IoIosMenu>: <IoMdClose className='w-8 h-8 cursor-pointer hidden max-md:block' onClick={()=>setIsOpen(!isOpen)}></IoMdClose>}
+    <nav className="w-screen h-16 flex items-center z-50 sticky backdrop-blur-sm bg-NavColor">
+      <div className="flex items-center w-full justify-between px-20 max-sm:px-10">
+        <h1 className="font-bold text-4xl max-md:text-xl">Giacomo Roberti</h1>
+        <div className="flex gap-10 max-md:hidden">
+          <Link href={"#about"}>About me</Link>
+          <Link href={"#servizi"}>Services</Link>
+          <Link href={"#contatti"}>Contact me</Link>
         </div>
-        {isOpen && <div className='absolute top-16 flex flex-col gap-5 bg-NavColor opacity-trans border-b border-white w-screen px-20 py-9 max-sm:px-10 '>
-                <Link href={'#about'}>About me</Link>
-                <Link href={'#servizi'}>Sevices</Link>
-                <Link href={'#contatti'}>Contact me</Link>
+        {!isOpen ? (
+          <IoIosMenu
+            className="w-8 h-8 cursor-pointer hidden max-md:block"
+            onClick={handleMenuToggle}
+          ></IoIosMenu>
+        ) : (
+          <IoMdClose
+            className="w-8 h-8 cursor-pointer hidden max-md:block"
+            onClick={handleMenuToggle}
+          ></IoMdClose>
+        )}
+      </div>
+      {isOpen && (
+        <div
+          className={`absolute top-16 flex flex-col gap-5 bg-NavColor border-b border-white w-screen px-20 py-9 max-sm:px-10 ${
+            isClosing ? "opacity-trans-close" : "opacity-trans"
+          }`}
+        >
+          <Link href={"#about"}>About me</Link>
+          <Link href={"#servizi"}>Services</Link>
+          <Link href={"#contatti"}>Contact me</Link>
         </div>
-        }
-       
+      )}
     </nav>
-  )
+  );
 }
